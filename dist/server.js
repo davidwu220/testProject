01a0df3511f90ed1d095f23d2037baa279244bab
David Wu <davidwu@BanapaMac.lan>
2017-04-26 22:35:43 -0700

    cleaning up

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _config = require('config.js');
-
-var _config2 = _interopRequireDefault(_config);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-console.log(_config2.default);
