2546e18f283c6e0fb6c02b86ecb245331d738af0
BANAPAPC\david <david@wudavid.com>
2017-04-28 03:24:06 -0700

    Ferther setting up the project and work flow

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+        <h2 className="text-center">
+        Hello React With JSX! -- {Math.random()}
+        </h2>,
+        document.getElementById('root')
+);
