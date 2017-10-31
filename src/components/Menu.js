d7e927ebf9ce124a875f5d6e922033eb733915a8
David Wu <david@wudavid.com>
2017-10-30 17:03:46 -0700

    style fix

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -21,9 +21,9 @@ class Menu extends Component {
             this.props.onHomeMenuClick("");
         }
 
-        $(menuButton).addClass("active");
+        $(menuButton).addClass("is-active");
         if(this.previousSelection != "" && button != this.previousSelection) {
-            $(this.previousSelection).removeClass("active");
+            $(this.previousSelection).removeClass("is-active");
         }
         this.previousSelection = menuButton;
 
@@ -49,15 +49,14 @@ class Menu extends Component {
         return (
             <div className="Menu sticky-top">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="/">星島日報</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <a className="nav-link" id="home-btn" onClick={() => this.handleClick("body")}>星島廣告首頁 <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" id="home-btn" onClick={() => this.handleClick("body")}>首頁 <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" id="classified-ad-btn" onClick={() => this.handleClick("#classified-ad")}>星島分類廣告</a>
@@ -69,10 +68,10 @@ class Menu extends Component {
                             <a className="nav-link" href="https://www.singtaousa.com/la/" target="_blank">星島新聞首頁</a>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
+                    <div className="form-inline my-2 my-lg-0" onSubmit={(event) => event.preventDefault()}>
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.props.onSearchChange}/>
-                        <button className="btn btn-outline-success my-2 my-sm-0 d-none" type="submit" onSubmit={this.props.onSearch}>Search</button>
-                    </form>
+                        <button className="btn btn-outline-success my-2 my-sm-0 d-none" type="submit">Search</button>
+                    </div>
                     </div>
                 </nav>
             </div>
