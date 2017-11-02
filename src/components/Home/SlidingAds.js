9b8a564188f90e31a8c0a748d788df681dcca1a0
David Wu <david@wudavid.com>
2017-11-02 15:52:00 -0700

    some style change

--- src/components/Home/SlidingAds.js
+++ src/components/Home/SlidingAds.js
@@ -11,35 +11,27 @@ class SlidingAds extends Component {
             <div className="carousel" data-flickity='{ "lazyLoad": 2, "autoPlay": true, "imagesLoaded": true, "wrapAround": true }'>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="First slide" />
-   
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="Second slide" />
-       
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="Third slide" />
-
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="First slide" />
-
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="Second slide" />
-    
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="Third slide" />
-
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="First slide" />
-
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="Second slide" />
-
                 </div>
                 <div className="carousel-cell">
                     <img className="carousel-cell-image" data-flickity-lazyload="/images/page_icons/placeholder.jpg" alt="Third slide" />
